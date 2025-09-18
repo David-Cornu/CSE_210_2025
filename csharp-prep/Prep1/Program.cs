df83a786c47d9d98d6c11534ed97f368e1e57ce7
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2025-09-18 11:55:47 -0700

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.IO.Pipes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string _fName = "none";
+        string _lName = "none";
+
+        Console.Write("What is your first name? : ");
+        _fName = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.Write("What is your last name? : ");
+        _lName = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {_lName}. {_fName} {_lName}");
     }
 }
