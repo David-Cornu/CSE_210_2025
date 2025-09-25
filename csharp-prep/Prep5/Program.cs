2f380af397112a533b53f11d2e11f25245105ca8
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2025-09-24 23:34:19 -0700

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string userName;
+        int userNumber;
+        string inputNumber;
+        int userBirthYear;
+        string inputYear;
+        int curentYear = DateTime.Now.Year;
+        int age;
+
+        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine();
+
+        Console.Write("What is your name? ");
+        userName = Console.ReadLine();
+        Console.Write("What is your favorite number? ");
+        inputNumber = Console.ReadLine();
+        userNumber = int.Parse(inputNumber);
+        Console.Write("What year were you born? ");
+        inputYear = Console.ReadLine();
+        userBirthYear = int.Parse(inputYear);
+
+        userNumber *= userNumber;
+        age = curentYear - userBirthYear;
+
+        Console.WriteLine("");
+        Console.WriteLine($"{userName} the square of your number is {userNumber}");
+        Console.WriteLine($"{userName} you will turn {age} this year.");
     }
 }
