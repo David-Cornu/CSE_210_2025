ade1d988d679933a6afc7466c82af34a078f5f29
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2025-09-24 23:13:05 -0700

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string userInput;
+        float userNumber = 1;
+        float numberCount = -1;
+        float average;
+        float total = 0;
+        float highestNumber = 0;
+        List<float> numbers = new List<float>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            userNumber = float.Parse(userInput);
+
+            numbers.Add(userNumber);
+        }
+
+        foreach (int n in numbers)
+        {
+            total += n;
+            numberCount += 1;
+            if (n > highestNumber)
+            {
+                highestNumber = n;
+            }
+        }
+
+        average = total / numberCount;
+
+        Console.WriteLine($"The total is: {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The highest number is {highestNumber}");
+
     }
 }
